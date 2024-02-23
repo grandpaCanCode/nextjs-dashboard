9fa3d5edd603d9a3b2bcbf544201deda184909f8
Mason <128002915+stringsArraysObjects@users.noreply.github.com>
2024-02-22 21:17:26 -0500

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,6 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
 
-
 export const metadata: Metadata = {
   title: 'login',
 };
